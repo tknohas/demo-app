4c45cafc578d2523c5a996513a37c1f7c113fe97
{tknohas} <0xv17851726325v@gmail.com>
2026-04-02 23:39:09 +0900

    プラグインやライブラリの導入

--- src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
+++ src/main/java/com/example/demoapp/presentation/backingbean/ReportNewBean.java
@@ -2,26 +2,16 @@ package com.example.demoapp.presentation.backingbean;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 @Named
 @RequestScoped
+@Getter
+@Setter
 public class ReportNewBean {
+  @NotBlank(message="タイトルを入力してください。")
   private String title;
   private String detail;
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDetail() {
-    return detail;
-  }
-
-  public void setDetail(String detail) {
-    this.detail = detail;
-  }
 }
